e6eaf0a371c4acd40bcc095472f9bf915ec75cc6
micky rendon <elderendon@hotmail.es>
2021-09-06 10:27:32 -0300

    arreglos para ingresos y egresos

---
+++ js/arrays.js
@@ -0,0 +1,10 @@
+const dataEntry = [
+    new Entry('salario', 2100.00),
+    new Egress('venta coche', 1500)
+];
+
+const dataEgress = [
+    new Egress('renta',  230.00),
+    new Egress('impuestos',  400.00),
+];
+
