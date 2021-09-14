4da626ba30a508d81f820fc896206979e7ce32df
micky rendon <elderendon@hotmail.es>
2021-09-13 22:09:02 -0300

    adding class close al span funcion createEntry

--- js/ingresosDinamicos.js
+++ js/ingresosDinamicos.js
@@ -22,7 +22,7 @@ const createEntry = (value) => {
                 <label for="numero">
                     <input id="numero" type="text" value="${currencyCoin(value.value)}">
                 </label>
-                <span>x</span>
+                <span class="close">x</span>
             </div>
         </li> `;
         return newBlock;
