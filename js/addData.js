d2ad1b3f6d6182ca14aaceff71211805ae15e87d
micky rendon <elderendon@hotmail.es>
2021-09-20 16:17:41 -0300

    agregar nuevos elementos a ingreso o egreso

---
+++ js/addData.js
@@ -0,0 +1,21 @@
+ let addData = () => {
+     let formulario = document.forms['new-item'];
+     let selectType = formulario['selector'];
+     let newDescription = formulario['input-description'];
+     let newValue = formulario['input-number'];
+
+     if(newDescription.value !== '' &&  newValue.value !== ''){
+         if(selectType.value === 'ingreso'){
+                // + = number()/ convirtiendo un string en number
+            dataEntry.push(new Entry(newDescription.value, +newValue.value));
+            HeaderonLoad();
+            runningDataEntry();
+         }else if(selectType.value === 'egreso'){
+            dataEgress.push(new Egress(newDescription.value, +newValue.value));
+            HeaderonLoad();
+            runningDataEgress();
+         };
+     }
+ };
+
+document.getElementById('btn-add').addEventListener('click', addData, true);
